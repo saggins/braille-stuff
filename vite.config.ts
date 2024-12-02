7f74695ff1127e3f555ff10a9fd67db7b0fa8e5c
Sangmin Chun <sangmin@sagg.in>
2024-12-02 14:51:32 -0800

    reading is hard

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit()]
+});
